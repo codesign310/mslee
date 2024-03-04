4a7feb3a1b097f09b98f67680a9d15b7f16476f9
codesign310 <161411345+codesign310@users.noreply.github.com>
2024-03-04 18:24:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def contents():
 
 
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
-
-
 if __name__ == '__main__':
-   app.run(debug=False, port=5000)
+    app.run(debug=True)
+
+
+#if __name__ == '__main__':
+#   app.run(debug=False, port=5000)
